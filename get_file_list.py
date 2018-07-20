c6f856490512db7245ff74ec5da692c89b7bb7b4
GCP03 <40214947+GCP03@users.noreply.github.com>
2018-07-19 21:30:15 -0400

    Update get_file_list.py

--- get_file_list.py
+++ get_file_list.py
@@ -1,14 +1,28 @@
-from os import listdir,stat
-from os.path import isfile, join
+import os
+#mypath="f:\\music\\"
+mypath=input("Enter directory to scan (e.g. c:) : ")
+outfile="c:\\users\\glenn\\filelist.csv"
+outfile=input("Enter name for output (e.g. c:\filelist.txt)")
 
+scan_dir(mypath)
 
-
-def get_files(mypath):
-    for f in listdir(mypath):
-        if isfile(join(mypath, f)) and stat(join(mypath, f)).st_size > 0:
-            yield f
-
-file_name=get_files("c:\\users\\glenn\\")
-out_file="c:\\users\\glenn\\filelist.csv"
-with open(out_file, mode='w+') as f:
-    f.write(next(file_name) + ',')
+def scan_dir(dir):
+    try:
+        with open(outfile, mode='w+') as f:
+            try:
+                for name in os.listdir(dir):
+                    #use this if you want the path along with filename
+                    path = os.path.join(dir, name)
+                    if os.path.isfile(path):
+                        print (path)
+                        f.write(path + '\n')
+                    else:
+                        scan_dir(path)
+            except FileNotFoundError:
+                print(f"Error! Can't open directory {dir}, please check directory name and try again.")
+            except:
+                print("Error!  Try again")
+    except:
+        print("Error opening file for write.")
+    else:
+        print(f"\nOutput written to {outfile}")
