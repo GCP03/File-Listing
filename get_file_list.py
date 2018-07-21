91f5f0acaa55492476ea7448c903214aa5608108
GCP03 <40214947+GCP03@users.noreply.github.com>
2018-07-20 22:59:39 -0400

    Update get_file_list.py

--- get_file_list.py
+++ get_file_list.py
@@ -1,28 +1,33 @@
+#TODO: Add filtering on filetype
+
 import os
 #mypath="f:\\music\\"
 mypath=input("Enter directory to scan (e.g. c:) : ")
 outfile="c:\\users\\glenn\\filelist.csv"
-outfile=input("Enter name for output (e.g. c:\filelist.txt)")
+outfile=input("Enter name for output (e.g. c:\\users\\glenn\\filelist.txt)")
 
-scan_dir(mypath)
-
-def scan_dir(dir):
+def scan_dir(dir, f):
     try:
-        with open(outfile, mode='w+') as f:
-            try:
-                for name in os.listdir(dir):
-                    #use this if you want the path along with filename
-                    path = os.path.join(dir, name)
-                    if os.path.isfile(path):
-                        print (path)
-                        f.write(path + '\n')
-                    else:
-                        scan_dir(path)
-            except FileNotFoundError:
-                print(f"Error! Can't open directory {dir}, please check directory name and try again.")
-            except:
-                print("Error!  Try again")
-    except:
-        print("Error opening file for write.")
-    else:
-        print(f"\nOutput written to {outfile}")
+        for name in os.listdir(dir):
+            #use this if you want the path along with filename
+            path = os.path.join(dir, name)
+            
+            #check if the item is a file with size > 0
+            if os.path.isfile(path) and os.path.getsize(path)>0:
+                print (path + ' size of: ' + str(os.path.getsize(path)))
+                #write the filename to the file
+                f.write(path + '\n')
+            else:
+                #we hist a subdirectory, so recurse and rescan
+                scan_dir(path, f)
+    except FileNotFoundError:
+        print(f"Error! Can't open directory {dir}, please check directory name and try again.")
+
+try:
+    f=open(outfile, mode='w+') 
+except:
+    print("Error opening file for write.")
+    
+scan_dir(mypath,f)
+f.close()
+print(f"\nOutput written to {outfile}")
